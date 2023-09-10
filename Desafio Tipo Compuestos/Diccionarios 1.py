c45faa1ad03dfaf8ec1387885d2143c6df5dc9d3
cchamom <134975805+cchamom@users.noreply.github.com>
2023-09-09 19:19:13 -0600

    Subí la tarea
    Subí los códigos con sus capturas

---
+++ Desafio Tipo Compuestos/Diccionarios 1.py
@@ -0,0 +1,8 @@
+def imprimir(paises):
+    for clave in paises:
+        print(clave,paises[clave])
+
+# bloque principal
+
+paises={"argentina":40000000, "españa'":4600000, "brasil":1900000, "uruguay":3400000}
+imprimir(paises)
---
+++ Desafio Tipo Compuestos/Lista-Ingresar Valores.py
@@ -0,0 +1,8 @@
+lista = []
+
+for X in range(5):
+    valor = int(input("Ingrese un valor entero: "))
+    lista.append(valor)
+
+print(lista)
+
